7e1d8665a27033a3c1913d1e6e6c5f78e8c8e56c
gitTest <caozhenwei8888@gmail.com>
2014-02-27 11:21:54 +0800

    dsfas

---
+++ zwGitTest/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  zwGitTest
+//
+//  Created by CaoZhenwei on 14-2-27.
+//  Copyright (c) 2014年 CaoZhenwei. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
