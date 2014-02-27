933904ee9f72555a59709cd91e3b895eb0db28fc
gitTest <caozhenwei8888@gmail.com>
2014-02-27 10:52:28 +0800

    Initial Commit

---
+++ zwGitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zwGitTest
+//
+//  Created by CaoZhenwei on 14-2-27.
+//  Copyright (c) 2014年 CaoZhenwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zwGitTestTests/zwGitTestTests.m
@@ -0,0 +1,34 @@
+//
+//  zwGitTestTests.m
+//  zwGitTestTests
+//
+//  Created by CaoZhenwei on 14-2-27.
+//  Copyright (c) 2014年 CaoZhenwei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface zwGitTestTests : XCTestCase
+
+@end
+
+@implementation zwGitTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
